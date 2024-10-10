ab6092fce5a12862545231b22adb852de12904f2
davestroud <david@davidstroud.me>
2024-10-10 11:16:08 -0400

    add linear regression

---
+++ ml_algorithms/linear_regression.py
@@ -0,0 +1,11 @@
+x = [1,2, 3, 4, 5]
+y = [2, 4, 5, 8, 10]
+
+mean_x = sum(x) / len(x)
+mean_y = sum(y) / len(y)
+
+numerator = sum((xi - mean_x) * (yi - mean_y) for xi, yi in zip(x, y))
+denominator = sum((xi - mean_x) ** 2 for xi in x)
+
+beta_1 = numerator / denominator
+beta_0 = mean_y - beta_1 * mean_x
